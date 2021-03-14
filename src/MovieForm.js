0d9b00bcd496a39fabaec653b38dbc761fdeb2fe
Gwenishere <gwenteste@yahoo.fr>
2021-03-14 22:41:56 +0100

    installation usestate

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
 import "./style.css";
 
 export default function MovieForm() {
+  
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+
   return (
     <div>
       <p>Movie form</p>
@@ -9,10 +14,16 @@ export default function MovieForm() {
         <label>Movie Name:</label>
         <input type="text"
         name="movieName"
+        onChange={(e)=> {
+          setMovieName(e.target.value)
+        }}
         placeholder="type a name"/>
         <label>Movie review:</label>
         <input type="text"
         name="review"
+        onChange={(e)=> {
+          setReview(e.target.value)
+        }}
         placeholder="type a review"/>
       </div>
       <div className='buttonForm'>
